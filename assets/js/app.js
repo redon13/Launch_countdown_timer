dd94878071c1889d2fa13706f56f3a1a0c4a0373
George Lakatos <redon13@gmail.com>
2021-02-17 19:09:04 +0000

    design completed

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+
+
+const title = document.querySelector('.title');
+    title.textContent = title.textContent.toUpperCase();
+
+const labels = document.querySelectorAll('.label');
+
+    labels.forEach(text => {
+       text.textContent = text.textContent.toUpperCase();
+    });
+
+
+
+
